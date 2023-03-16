b4fe3696d2a1bfb68bcdededd048aa4a8636cf94
vPilug <pilug.vero@gmail.com>
2023-03-16 18:56:26 +0200

    Added nearHundred method and methods comments

--- src/main/java/org/example/StringExercises.java
+++ src/main/java/org/example/StringExercises.java
@@ -15,8 +15,18 @@ public class StringExercises
         System.out.println(diff21(10));  //→ 11
         System.out.println(diff21(21));  //→ 0
         System.out.println("-".repeat(30));
+
+        System.out.println("Test nearHundred");
+        System.out.println(nearHundred(93));  //→ true
+        System.out.println(nearHundred(90));  //→ true
+        System.out.println(nearHundred(89));  //→ false
+        System.out.println("-".repeat(30));
     }
 
+    /*
+        The parameter weekday is true if it is a weekday, and the parameter vacation is true if we are on vacation.
+        We sleep in if it is not a weekday or we're on vacation. Return true if we sleep in.
+    */
     public static boolean sleepIn(boolean weekday, boolean vacation) {
         if(!weekday || vacation) {
             return true;
@@ -24,6 +34,9 @@ public class StringExercises
         return false;
     }
 
+    /*
+        Given an int n, return the absolute difference between n and 21, except return double the absolute difference if n is over 21.
+     */
     public static int diff21(int n) {
         if(n <= 21) {
             return 21 - n;
@@ -32,4 +45,11 @@ public class StringExercises
         }
     }
 
+    /*
+            Given an int n, return true if it is within 10 of 100 or 200.
+     */
+    public static boolean nearHundred(int n) {
+        return ((Math.abs(100 - n) <= 10) ||
+                (Math.abs(200 - n) <= 10));
+    }
 }
